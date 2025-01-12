add651d7e5cd32b74fa578b052dcfcda57bef64f
Rafael Paez <civic.vtieg6@outlook.com>
2025-01-12 15:20:58 -0300

    js de vdd

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,10 @@
+var velo = 79
+console.log(`a velocidade do seu carro é ${velo}km/h`)
+
+if (velo > 60) {
+    console.log('voce ultrapassou a velocidade permitida. MULTADO!')
+}
+
+else {
+    console.log('voce esta dentro da velocidade permitida. PARABENS!')
+}
