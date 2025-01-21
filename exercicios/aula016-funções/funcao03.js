e8e266e9832d9e73c3d4fa6be7dc8d5ea7976ab6
Rafael Paez <civic.vtieg6@outlook.com>
2025-01-21 13:00:10 -0300

    fix

--- exercicios/aula016-funções/funcao03.js
+++ exercicios/aula016-funções/funcao03.js
@@ -1,9 +1,9 @@
-fatorial(n) {
-    let fat = 1
-    for (let c=n; c > 1; c--) {
-        fat *= c
+function fatorial(n) {
+    let fat = 1;
+    for (let c = n; c > 1; c--) {
+        fat *= c;
     }
-    return'fat'
+    return fat;
 }
 
-console.log(v(5))
+console.log(fatorial(5));
